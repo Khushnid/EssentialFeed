e50e08322bb10877944b0778f0a955ea0f7246c3
Khushnidjon <khushnidgit@gmail.com>
2023-02-06 18:00:18 +0500

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Khushnidjon on 06/02/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

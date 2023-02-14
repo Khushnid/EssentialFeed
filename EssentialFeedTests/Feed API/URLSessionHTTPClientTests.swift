7e0a09d9ca9bdc67a90c7dde6230569c4d000a7e
Khushnidjon <khushnidgit@gmail.com>
2023-02-14 14:20:41 +0500

    Improving tests according to the need of new SDK version(14.0 +)

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -36,7 +36,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { result in
             switch result {
             case let .failure(recievedError as NSError):
-                XCTAssertEqual(recievedError, error)
+                XCTAssertEqual(recievedError.domain, error.domain)
             default:
                 XCTFail("Expected failure with error \(error), got \(result) instaed")
             }

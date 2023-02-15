c1d9a43e19f9c0f28038a72673a8a263a39a8c8a
Khushnidjon <khushnidgit@gmail.com>
2023-02-15 10:17:58 +0500

    Move the URLSessionHTTPClient (system under tests) creating to a factory method to protect our tests from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -45,7 +45,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT() .get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -55,10 +55,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "Request Error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil,  error: error)
         
-        let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT() .get(from: url) { result in
             switch result {
             case let .failure(recievedError as NSError):
                 XCTAssertEqual(recievedError.domain, error.domain)
@@ -72,7 +71,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    // MARK: - Heplers
+    // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?

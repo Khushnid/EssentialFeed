024a1d09ea11512b59e6fc23d66f96ccee297c6c
Khushnidjon <khushnidgit@gmail.com>
2023-03-07 10:37:05 +0500

    Clean up & undo all cache side effects on 'setup' 'teardown' to avoid sharing state between tests

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,20 +9,31 @@ import XCTest
 import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
-
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
-
+        
         let exp = expectation(description: "Wait for load completion")
         sut.load { result in
             switch result {
             case let .success(imageFeed):
                 XCTAssertEqual(imageFeed, [], "Expected empty feed")
-
+                
             case let .failure(error):
                 XCTFail("Expected successful feed result, got \(error) instead")
             }
-
+            
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
@@ -38,13 +49,24 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(for: sut, store, file: file, line: line)
         return sut
     }
-
+    
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
     }
-
+    
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
-
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
 }

d6830a5a2ae7a1826545d8a8b3c33ab8344c41a5
Khushnidjon <khushnidgit@gmail.com>
2023-04-25 16:25:44 +0500

    Hide error message on tap

--- EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -11,13 +11,30 @@ public final class ErrorView: UIView {
     @IBOutlet private var label: UILabel!
     
     public var message: String? {
-        get { return label.text }
-        set { label.text = newValue }
+        get { return alpha > 0 ? label.text : nil }
+        set { setMessageAnimated(newValue) }
     }
     
     public override func awakeFromNib() {
         super.awakeFromNib()
         
         label.text = nil
+        alpha = 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        guard let message else { return hideMessageAnimated() }
+        label.text = message
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    @IBAction private func hideMessageAnimated() {
+        UIView.animate(withDuration: 0.25, animations: { self.alpha = 0 }, completion: { done in
+            guard done else { return }
+            self.label.text = nil
+        })
     }
 }
